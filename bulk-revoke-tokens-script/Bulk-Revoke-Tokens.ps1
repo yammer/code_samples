899afc0a31c57a5ee8cf07dbca2f83c9b7b0fd55
meizhi hu <yimeng1985@gmail.com>
2019-10-23 11:40:53 -0700

    powershell script to call yammer api to bulk revoke tokens

---
+++ bulk-revoke-tokens-script/Bulk-Revoke-Tokens.ps1
@@ -0,0 +1,59 @@
+param (
+  [Parameter(Mandatory=$true)][string]$CsvPath,
+  [Parameter(Mandatory=$true)][string]$BearerToken
+)
+
+function RequestDeletion {
+  param (
+    [string]$Json,
+    [string]$BearerToken,
+    [string]$Api
+  )
+  $Headers = @{Authorization=("Bearer {0}" -f $BearerToken)}
+  try
+  {
+    $Response = Invoke-RestMethod -Uri $Api -Method POST -ContentType "application/json" -Body $Json -Headers $Headers
+    @{StatusCode=$Response.BaseRequest.StatusCode; Content=$Response.Content} | Format-Table -AutoSize
+  } catch {
+    @{StatusCode=[int]$_.Exception.Response.StatusCode; Content=$_.ErrorDetails.Message} | Format-Table -AutoSize
+  }
+}
+
+$BatchSize = 100
+$UserIdColName = "user_id"
+
+$CsvObj = Import-Csv -Path $CsvPath
+
+if ($CsvObj | Get-Member -Name $UserIdColName -MemberType NoteProperty)
+{
+  $UserIds = $CsvObj.$UserIdColName
+  if ($UserIds.Count -gt 0)
+  {
+    $Api = "https://www.yammer.com/api/v1/tokens/deletion"
+    $Batch = New-Object Collections.Generic.List[long]
+
+    For ($i = 0; $i -lt $UserIds.Count; $i++)
+    {
+      $Batch.Add([long]($UserIds[$i]))
+
+      if ($Batch.Count -ge $BatchSize)
+      {
+        $Json = ('{{"user_ids": [{0}]}}' -f ($Batch -join ","))
+        RequestDeletion -Json $Json -BearerToken $BearerToken -Api $Api
+        $Batch.Clear()
+      }
+    }
+
+    if ($Batch.Count -gt 0)
+    {
+      $Json = ('{{"user_ids": [{0}]}}' -f ($Batch -join ","))
+      RequestDeletion -Json $Json -BearerToken $BearerToken -Api $Api
+      $Batch.Clear()
+    }
+  }
+}
+else {
+  Write-Error ("Cannot find column {0} in file {1}" -f $UserIdColName, $CsvPath)
+  exit 2
+}
+
---
+++ bulk-revoke-tokens-script/README.md
@@ -0,0 +1,26 @@
+# bulk-revoke-yammer-token
+This is a powershell script Sample which will invoke Yammer public API to bulk revoke Yammer legacy token.
+
+## Usage
+```
+bulk-revoke-yammer-token.ps1 [-CsvPath] <string> [-BearerToken] <string> [<CommonParameters>]
+```
+This script takes two parameters to run
+* CsvPath: The path of the CSV file which contains "user_id" column
+* BearerToken: AAD token string which will be used to do authentication
+
+## Yammer API
+POST: https://www.yammer.com/api/v1/tokens/deletion 
+
+This API will delete all the yammer bearer tokens for the users which provided in the post data "user_ids".
+
+A curl command example to call this API:
+```
+curl -v -d '{"user_ids":[1,2,3]}' -H "Content-Type: application/json" -H "Authorization: Bearer <AAD_Token>" -X POST https://www.yammer.com/api/v1/tokens/deletion
+```
+
+## Work Flow
+1. Get all the user ids from the csv file
+2. Invoke the post API to revoke tokens for at most 100 users each time
+
+
